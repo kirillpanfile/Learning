34dcd663ccf35dbb7e0d5d510518322b264e69c5
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2021-12-19 20:31:23 +0200

    Sloved Kata in TypeScript
    Made first steps in learning TypeScript

--- Square Every Digit/script.ts
+++ Square Every Digit/script.ts
@@ -1,7 +1,5 @@
 export class Kata {
   static squareDigits(num: number): number {
-       return Number(String(num).split("").map((num) => {
-        return Math.floor(Math.pow(Number(num), 2));
-    }).join(''));
+       return Number(String(num).split("").map((num) => Math.floor(Math.pow(Number(num), 2))).join(''));
   }
 }
