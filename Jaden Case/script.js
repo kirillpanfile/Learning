39e2919a3cc485b58df06b54c15e8a84a73a17df
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2021-12-20 16:35:34 +0200

    Removed Testing

--- Jaden Case/script.js
+++ Jaden Case/script.js
@@ -1,7 +1,3 @@
 String.prototype.toJadenCase = function () {
     return this.split(' ').map(el => el[0].toUpperCase() +el.slice(1)).join(' ');
 };
-
-let str = "How can mirrors be real if our eyes aren't real";
-
-console.log(str.toJadenCase());
--- Square Every Digit/script.js
+++ Square Every Digit/script.js
@@ -1,8 +1,3 @@
 function squareDigits(num){
-    return Number(String(num).split("").map((num) => {
-        return Math.floor(Math.pow(Number(num), 2));
-    }).join(''));
+    return Number(String(num).split("").map(num => Math.floor(Math.pow(Number(num), 2))).join(''));
 }
-
-
-console.log(squareDigits(3212));
