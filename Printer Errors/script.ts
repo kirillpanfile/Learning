b0342a6cbd146280883393bd03b1b15f634f4017
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2021-12-20 16:33:00 +0200

    optmized code

--- Printer Errors/script.ts
+++ Printer Errors/script.ts
@@ -1,13 +1,7 @@
 export class G964 {
     public static printerError(s: string): string {
         let invalid: number = 0;
-        s.split('').forEach((element) => {
-            if (element > "m") 
-                invalid++     
-    })
-    console.log();
+        s.split('').forEach((element) => {if (element > "m") invalid++})
     return `${invalid}/${s.length}`
     }
 }
-
-console.log(G964.printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"));
