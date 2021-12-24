cf61b3751c5a05ec98d32c422082cd67090b88fb
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2021-12-24 11:41:35 +0200

    Update strig.ts

--- Methods/strig.ts
+++ Methods/strig.ts
@@ -2,13 +2,17 @@ let stringOne: string = "This is the first string"
 let stringTwo: string = "This is the second string"
 
 // charAt()
-console.log(stringOne.charAt(0));
+console.log(stringOne.charAt(0)); //* T
 
 //charCodeAt()
-console.log(stringOne.charCodeAt(0));
+console.log(stringOne.charCodeAt(0)); //* 84
 
 //concat()
-console.log(stringOne.concat(stringTwo));
+console.log(stringOne.concat(stringTwo)); //* This is the first stringThis is the second string
 
 //endWith()
-console.log(stringOne.endsWith("ng"));
+console.log(stringOne.endsWith("ng")); //* true
+
+// fromCharCode()
+
+console.log(String.fromCharCode(84)); //* T
