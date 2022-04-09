075a4a1e0bfeba6bad44ffbc0968b9a3ad4656ba
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-04-09 13:39:27 +0300

    Create app.js

---
+++ CodeWars/Sum Of Functions/app.js
@@ -0,0 +1,14 @@
+const zero = (op) => (op ? op(0) : 0);
+const one = (op) => (op ? op(1) : 1);
+const two = (op) => (op ? op(2) : 2);
+const three = (op) => (op ? op(3) : 3);
+const four = (op) => (op ? op(4) : 4);
+const five = (op) => (op ? op(5) : 5);
+const six = (op) => (op ? op(6) : 6);
+const seven = (op) => (op ? op(7) : 7);
+const eight = (op) => (op ? op(8) : 8);
+const nine = (op) => (op ? op(9) : 9);
+const plus = (value) => (data) => data + value;
+const minus = (value) => (data) => data - value;
+const times = (value) => (data) => data * value;
+const dividedBy = (value) => (data) => Math.floor(data / value);
