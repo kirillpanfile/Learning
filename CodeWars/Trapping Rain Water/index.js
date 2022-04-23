336c27cc83389287ebc86e953fc0ca1c23ee85eb
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-04-23 19:00:56 +0300

    Create index.js

---
+++ CodeWars/Trapping Rain Water/index.js
@@ -0,0 +1,19 @@
+let trap = function (height) {
+  let max = (left = leftMax = rightMax = 0),
+    right = height.length - 1;
+  while (left < right) {
+    height[left] < height[right]
+      ? (height[left] >= leftMax
+          ? (leftMax = height[left])
+          : (max += leftMax - height[left]),
+        left++)
+      : (height[right] >= rightMax
+          ? (rightMax = height[right])
+          : (max += rightMax - height[right]),
+        right--);
+  }
+  return max;
+};
+
+console.log(trap([4, 2, 0, 3, 2, 5]));
+console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
